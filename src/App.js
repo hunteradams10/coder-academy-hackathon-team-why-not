8ef99944c9b3dc8d792527df993c5a685ccd9fcd
Hunter Adams <hunteradams10@gmail.com>
2022-09-19 20:34:57 +1000

    added darkmode and some styling to main page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
@@ -5,21 +6,40 @@ import EmojiSearch from "./components/EmojiSeach";
 import HireUs from "./components/HireUs";
 import NotFound from "./components/NotFound";
 import SearchBar from "./components/SearchBar";
+import { createContext, useState } from 'react';
+import ReactSwitch from 'react-switch'
+
+export const ThemeContext = createContext(null)
 
 function App() {
-  return (
-    <div>
-        <Router>
-            <NavBar />
-        <Routes>  
-          <Route exact path='/' element={<EmojiSearch />} />
-          <Route path='/hireUs' element={<HireUs />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>    
-      </Router>
+  const [theme, setTheme] = useState("light");
 
-      <SearchBar />
-    </div>
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <div>
+          <Router>
+            <NavBar />
+            <Routes>
+              <Route exact path="/" element={<EmojiSearch />} />
+              <Route path="/hireUs" element={<HireUs />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Router>
+
+          <SearchBar />
+
+          <div className="switch">
+            <label>{theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+            <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+          </div>
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
