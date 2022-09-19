0a44012d813e5837f95c13dce4ae437f21d280a6
Nhi Tran <98810653+nhithutran@users.noreply.github.com>
2022-09-19 17:17:54 +1000

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import EmojiSearch from "./components/EmojiSeach";
 import HireUs from "./components/HireUs";
 import NotFound from "./components/NotFound";
-import SearchBar from "./SearchBar";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-
   return (
     <div>
         <Router>
-          <nav>
-            <ul>
-              <li><Link to='/'>EmojiSearch</Link></li>
-              <li><Link to='/HireUs'>Hire Us</Link></li>
-            </ul>
-          </nav>
+            <NavBar />
         <Routes>  
           <Route exact path='/' element={<EmojiSearch />} />
           <Route path='/hireUs' element={<HireUs />} />
