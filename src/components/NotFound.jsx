9dbb107199d10d96132a7fe2bc47c992de583fec
sofia_frugone <sofiabellaf2000@gmail.com>
2022-09-19 21:50:59 +1000

    pls work part two

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,8 +3,23 @@ import React from 'react'
 
 const NotFound = () => {
     return (
-      <div >
-        <h1>404 Page Note Found</h1>
+      <div className='notfound'>
+        <div className='notfound-text'>
+          <p>Sorry! The page you’re looking for cant be found.</p>
+       </div>
+
+       <div className='notfound-home'>
+         <p>Back to <a href=''>Home</a></p>
+       </div>
+
+       <div className='notfound-img'>
+         <img src='image/Su.png' alt='Su'></img>
+         <img src='image/Nhi.png' alt='Nhi'></img>
+         <img src='image/Hunter.png' alt='Hunter'></img>
+         <img src='image/Sofia.png' alt='Sofia'></img>
+       </div>
+        
+        
       </div>
     )
   }
