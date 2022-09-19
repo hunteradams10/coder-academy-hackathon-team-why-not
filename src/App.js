a162f0d9e86bd7aee707965d10ad928887997c56
Hunter Adams <90118549+hunteradams10@users.noreply.github.com>
2022-09-19 12:23:44 +1000

    Revert "added App.css back and installed React Switch using npm install react…"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import './App.css'
-import { createContext, useState } from 'react';
-import ReactSwitch from 'react-switch'
 function App() {
-  {
-
   return (
     null
-  )
+  );
 }
 
 export default App;
