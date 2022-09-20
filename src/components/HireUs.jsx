4c18dfdd3981af5af79ba254df8a7a3e48b1a4c4
sofia_frugone <sofiabellaf2000@gmail.com>
2022-09-20 10:59:53 +1000

    fixed styling on hire page added links and bio

--- src/components/HireUs.jsx
+++ src/components/HireUs.jsx
@@ -7,64 +7,73 @@ const HireUs = () => {
       <div className='hire-us'>
         
         <div className="hire-us-message">
-          <p>Looking for developers to join your team? Why not hire us as your next assets.</p>
+          <p>Looking for developers to join your team? <br></br>Why not hire us as your next assets.</p>
+          
         </div>
         
         <div className='bio-card-main'>
           <div className='bio-card'>
             <div className='bio-image'>
-              <img src='image/placeholder.png' alt='bio image'></img>
+              <img src='image/nhi-bio.png' alt='bio image'></img>
+            </div>
+            <div className='bio-icons'>
+              <a target='_blank' href='https://www.linkedin.com/in/nhi-tran-39955b233/'><img src='image/linked-in-logo.png'></img></a>
+              <a target='_blank' href='https://github.com/nhithutran'><img src='image/github-logo.png'></img></a>
             </div>
             <div className='bio-text'>
               <p>Nhi Tran</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+              <p>Junior Developer that specialises in MERN stack, Rails and Agile Methodology.  Experience Senior Quality Assurance and passionate about anything to do with Web Development.</p>
             </div>
-            <div className='bio-icons'>
-              <a href='#'><img src='image/linked-in-logo.png'></img></a>
-              <a href='#'><img src='image/github-logo.png'></img></a>
-            </div>
+            
           </div>
 
           <div className='bio-card'>
             <div className='bio-image'>
-              <img src='image/placeholder.png' alt='bio image'></img>
-            </div>
-            <div className='bio-text'>
-              <p>Nhi Tran</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+              <img src='image/su-bio.png' alt='bio image'></img>
             </div>
             <div className='bio-icons'>
-              <a href='#'><img src='image/linked-in-logo.png'></img></a>
-              <a href='#'><img src='image/github-logo.png'></img></a>
+              <a target='_blank' href='https://www.linkedin.com/in/su-zhang-au'><img src='image/linked-in-logo.png'></img></a>
+              <a target='_blank' href='https://codergirlsu.github.io/src/'><img src='image/github-logo.png'></img></a>
             </div>
+            <div className='bio-text'>
+              <p>Su Zhang</p>
+              <p>Hi, I'm an emerging full-stack Software Engineer and I specialise in web technologies such as React and node.js. My newfound passion for software engineering has given me many new opportunities to express my existing skills.</p>
+            </div>
+            
           </div>
 
           <div className='bio-card'>
             <div className='bio-image'>
-              <img src='image/placeholder.png' alt='bio image'></img>
-            </div>
-            <div className='bio-text'>
-              <p>Nhi Tran</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+              <img src='image/hunter-bio.png' alt='bio image'></img>
             </div>
             <div className='bio-icons'>
-              <a href='#'><img src='image/linked-in-logo.png'></img></a>
-              <a href='#'><img src='image/github-logo.png'></img></a>
+              <a target='_blank' href='https://www.linkedin.com/in/hunteradamsdev'><img src='image/linked-in-logo.png'></img></a>
+              <a target='_blank' href='https://github.com/hunteradams10'><img src='image/github-logo.png'></img></a>
             </div>
+            <div className='bio-text'>
+              <p>Hunter Adams</p>
+              <p>Hello! I am a growing web developer. Anything Javascript is my jam, and I have a soft spot for source control. </p>
+            </div>
+            
           </div>
 
           <div className='bio-card'>
             <div className='bio-image'>
-              <img src='image/placeholder.png' alt='bio image'></img>
+              <img src='image/fi-bio.png' alt='bio image'></img>
             </div>
-            <div className='bio-text'>
-              <p>Nhi Tran</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </div>
+
             <div className='bio-icons'>
-              <a href='#'><img src='image/linked-in-logo.png'></img></a>
-              <a href='#'><img src='image/github-logo.png'></img></a>
+              <a target='_blank' href='https://www.linkedin.com/in/sofiabellafrugone/'><img src='image/linked-in-logo.png'></img></a>
+              <a target='_blank' href='https://github.com/sofia-frugone'><img src='image/github-logo.png'></img></a>
             </div>
+
+            <div className='bio-text'>
+              <p>Sofia Frugone</p>
+              <p>I have over 3 years of experience working in a quality assurance-based role. I currently work for 365 Roadside Assistance as a part-time Junior Web Developer, improving email marketing and services for our customers and clients.
+
+              </p>
+            </div>
+            
           </div>
 
         </div>
