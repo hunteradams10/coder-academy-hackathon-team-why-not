d4c1b58bc8eb6d2bf70c5c0c1094e9f915e0e600
Hunter Adams <hunteradams10@gmail.com>
2022-09-20 10:13:19 +1000

    adjusted title and darkmode colours

--- src/components/EmojiSeach.jsx
+++ src/components/EmojiSeach.jsx
@@ -4,7 +4,7 @@ import '../App.css'
 function EmojiSearch() {
   return (
     <div className="heading">
-      <h1>Emoji Search</h1>
+      <h1>Searching for just the right emoji? Type a word and <span className="emojiword">EmojiSearch</span> will do the rest.</h1>
     </div>  
   )  
 }
