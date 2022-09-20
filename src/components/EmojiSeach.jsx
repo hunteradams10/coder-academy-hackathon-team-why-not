2c033664a6831be6a35e441883434e15ae7864ac
Hunter Adams <hunteradams10@gmail.com>
2022-09-20 11:41:52 +1000

    fixed search bar appearing on all pages

--- src/components/EmojiSeach.jsx
+++ src/components/EmojiSeach.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../App.css'
+import SearchBar from './SearchBar'
 
 function EmojiSearch() {
   return (
     <div className="heading">
       <h1>Searching for just the right emoji? Type a word and <span className="emojiword">EmojiSearch</span> will do the rest.</h1>
+      <SearchBar />
     </div>  
   )  
 }
